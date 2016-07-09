82314586d581427fa31073a94edc3348fc7dd300
Martin Rehder <apple01@rehsco.com>
2016-07-09 13:13:26 +0200

    Initial Commit

---
+++ MJRFlexStyleComponents/MJRFlexStyleComponents.h
@@ -0,0 +1,19 @@
+//
+//  MJRFlexStyleComponents.h
+//  MJRFlexStyleComponents
+//
+//  Created by Martin Rehder on 09.07.16.
+//  Copyright © 2016 Martin Rehder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MJRFlexStyleComponents.
+FOUNDATION_EXPORT double MJRFlexStyleComponentsVersionNumber;
+
+//! Project version string for MJRFlexStyleComponents.
+FOUNDATION_EXPORT const unsigned char MJRFlexStyleComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MJRFlexStyleComponents/PublicHeader.h>
+
+
