35d1e23f7f91c4cc2e0d94dd60ccbaf4f8fd7013
Martin Rehder <martin@Rehsco-MacPro.fritz.box>
2019-02-16 17:18:03 +0100

    Added commonly needed extension for FlexCollectionView

---
+++ MJRFlexStyleComponents/FlexCollectionViewExtensions.swift
@@ -0,0 +1,64 @@
+//
+//  FlexCollectionViewExtensions.swift
+//  MJRFlexStyleComponents
+//
+//  Created by Martin Rehder on 16.02.2019.
+/*
+ * Copyright 2018-present Martin Jacob Rehder.
+ * http://www.rehsco.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+import UIKit
+
+public extension FlexCollectionView {
+    public func getHorizontalSpacing() -> CGFloat {
+        if let layout = itemCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            return layout.scrollDirection == .vertical ? layout.minimumInteritemSpacing : layout.minimumLineSpacing
+        }
+        return 0
+    }
+    
+    public func getVerticalSpacing() -> CGFloat {
+        if let layout = itemCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            return layout.scrollDirection == .vertical ? layout.minimumLineSpacing : layout.minimumInteritemSpacing
+        }
+        return 0
+    }
+    
+    public func getHorizontalSectionInset(forSection sectionIdx: Int) -> CGFloat {
+        if let layout = itemCollectionView.collectionViewLayout as? UICollectionViewFlowLayout, let section = getSection(atIndex: sectionIdx) {
+            let inset = layout.sectionInset
+            let sInset = section.insets
+            return inset.left + inset.right + sInset.left + sInset.right
+        }
+        return 0
+    }
+    
+    public func getVerticalSectionInset(forSection sectionIdx: Int) -> CGFloat {
+        if let layout = itemCollectionView.collectionViewLayout as? UICollectionViewFlowLayout, let section = getSection(atIndex: sectionIdx) {
+            let inset = layout.sectionInset
+            let sInset = section.insets
+            return inset.top + inset.bottom + sInset.top + sInset.bottom
+        }
+        return 0
+    }
+}
